1f92d3bd14bff1d8bda2b6061f96ee7148becc60
Sergo_Kaz13 <fenrill1366613@gmail.com>
2022-08-16 22:18:10 +0300

    add onClick button and onChange input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { ThemeProvider } from 'styled-components';
 
 import { Layout } from './components';
+import { LayoutContainer } from './containers';
 import { GlobalStyles } from './styles';
 import { darkTheme } from './themes';
 
 export const App = () => (
     <ThemeProvider theme={darkTheme}>
         <GlobalStyles />
-        <Layout>
-            <span>Content</span>
-        </Layout>
+
+        <LayoutContainer>
+            {({ movies, ...other }) => (
+                <Layout {...other}>
+                    <span>Movies list:</span>
+
+                    {movies.map(movie => (
+                        <div key={movie.id}>Movie ID: {movie.id}</div>
+                    ))}
+                </Layout>
+            )}
+        </LayoutContainer>
     </ThemeProvider>
 );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
+import PT from 'prop-types';
+
 import { Input } from '../Input';
 import { Button } from '../Button';
 import { StyledWidthLimiter } from '../../styles';
 import { StyledHeader, StyledInputWrapper, StyledSearch } from './styles';
 
-export const Header = () => (
+export const Header = ({
+    search,
+    isSearching,
+    onChangeSearch,
+    onSearchMovies
+}) => (
     <StyledHeader>
         <StyledWidthLimiter>
             <StyledSearch>
@@ -12,11 +19,34 @@ export const Header = () => (
                         type="text"
                         name="search"
                         placeholder="Search movies"
+                        value={search}
+                        onChange={onChangeSearch}
                     />
                 </StyledInputWrapper>
 
-                <Button>Search</Button>
+                <Button disabled={isSearching} onClick={onSearchMovies}>
+                    {isSearching ? 'Searching' : 'Search'}
+                </Button>
             </StyledSearch>
         </StyledWidthLimiter>
     </StyledHeader>
 );
+
+Header.propTypes = {
+    /**
+     * Search input value
+     */
+    search: PT.string.isRequired,
+    /**
+     * Indicates if we are searching movies
+     */
+    isSearching: PT.bool.isRequired,
+    /**
+     * Handler for changing search input value
+     */
+    onChangeSearch: PT.func.isRequired,
+    /**
+     * Handler for searching movies
+     */
+    onSearchMovies: PT.func.isRequired
+};
