9f31d1ecbf174caba76ec4a3751859a0ab9a4fa7
Sergo_Kaz13 <fenrill1366613@gmail.com>
2022-08-20 22:42:22 +0300

    add file env

--- src/containers/LayoutContainer/LayoutContainer.js
+++ src/containers/LayoutContainer/LayoutContainer.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import axios from 'axios';
 
+const { REACT_APP_API_URL, REACT_APP_MOVIE_API_KEY } = process.env;
+
 export const LayoutContainer = ({ children }) => {
     const [search, setSearch] = useState('');
     const [isSearching, setIsSearching] = useState(false);
@@ -13,20 +15,33 @@ export const LayoutContainer = ({ children }) => {
     const handleSearchMovies = async () => {
         setIsSearching(true);
 
-        // fetch('https://jsonplaceholder.typicode.com/users')
-        //     .then(response => response.json())
-        //     .then(users => {
-        //         console.log(users);
-
-        //         setMovies(users);
-        //         setIsSearching(false);
-        //     });
         try {
-            const response = await fetch(
-                'https://jsonplaceholder.typicode.com/users'
+            const url = `${REACT_APP_API_URL}/search/movie?api_key=${REACT_APP_MOVIE_API_KEY}&language=en-US&query=${search}&page=1&include_adult=false`;
+
+            // https://api.the moviedb.org/3/search/movie?api_key=9e423027d44ac23fbe0304f005123b95&language=en-US&query=a&page=1&include_adult=false
+
+            // /3/search/movie?api_key=9e423027d44ac23fbe0304f005123b95&language=en-US&query=a&page=1&include_adult=false
+
+            const {
+                data: { results }
+            } = await axios.get(
+                'https://api.themoviedb.org/3/search/movie?api_key=9e423027d44ac23fbe0304f005123b95&language=en-US&query=a&page=1&include_adult=false'
             );
-            const users = await response.json();
-            setMovies(users);
+
+            console.log(url);
+            console.log(
+                'https://api.themoviedb.org/3/search/movie?api_key=9e423027d44ac23fbe0304f005123b95&language=en-US&query=a&page=1&include_adult=false'
+            );
+
+            console.log(results);
+
+            // fetch(
+            //     'https://api.themoviedb.org/3/search/movie?api_key=9e423027d44ac23fbe0304f005123b95&language=en-US&query=a&page=1&include_adult=false'
+            // )
+            //     .then(response => response.json())
+            //     .then(results => console.log(results));
+
+            setMovies(results);
             setIsSearching(false);
         } catch (e) {
             console.error(e);
